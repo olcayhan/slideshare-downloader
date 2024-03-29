daec81e87c3b1dc8a1bf8a88650747a8bf9e1354
olcayhan <kolcayhan1@gmail.com>
2024-03-29 15:35:37 +0300

    Add Tailwind CSS and React Spinners dependencies*** ***Update page component with loading spinner and input field*** ***Remove unused CSS styles from page module

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,29 +1,64 @@
 "use client";
 import axios from "axios";
 import React, { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Page = () => {
   const [data, setData] = useState<string[]>([]);
+  const [url, setUrl] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.post("http://localhost:5000/api/scrape", {
-        url: "https://www.slideshare.net/atkearney/pursuing-customer-inspired-growth",
+        url: url,
       });
       setData(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <button onClick={fetchData}>Fetch Data</button>
-      <hr />
-      <div>
-        {data.map((data) => (
-          <img src={data} />
-        ))}
+    <div className="w-full h-screen">
+      <div className="flex flex-col justify-start items-center w-full h-full mt-20 gap-4">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <p className="font-bold text-[40px]">SlideShare Downloader</p>
+          <p className="font-bold text-[24px] opacity-70">
+            Download Free Presentation
+          </p>
+        </div>
+        <div className="grid grid-cols-8 bg-blue-300 p-10 gap-5 max-w-[800px] w-full rounded-lg">
+          <input
+            type="text"
+            className="col-span-6 rounded-full p-3 outline-blue-500"
+            placeholder="Paste url here..."
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            className="col-span-2 bg-orange-300 p-2 rounded-xl disabled:opacity-50"
+            onClick={fetchData}
+            disabled={!url || isLoading}
+          >
+            {isLoading ? (
+              <ClipLoader
+                size={20}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            ) : (
+              "Fetch Data"
+            )}
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          {data.map((data) => (
+            <img src={data} />
+          ))}
+        </div>
       </div>
     </div>
   );
