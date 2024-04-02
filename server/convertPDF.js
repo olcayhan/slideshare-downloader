43d4fde445883b871f153fe0a91d26bce5263c05
olcayhan <kolcayhan1@gmail.com>
2024-04-02 18:59:56 +0300

    Update convertImagetoPdf function to use newSrc for image loading

--- server/convertPDF.js
+++ server/convertPDF.js
@@ -1,20 +1,17 @@
-const { createCanvas, loadImage } = require("canvas");
+const { createCanvas, Image, loadImage } = require("canvas");
 const { jsPDF } = require("jspdf");
 
 async function convertImagetoPdf(images) {
-  console.log("aldı");
   const pdf = new jsPDF();
-  console.log("aldı2");
   for (const resimURL of images) {
-    const img = await loadImage(resimURL.srcset);
+    const newSrc = resimURL.srcset.split(",")[1];
+    const img = await loadImage(newSrc);
     const canvas = createCanvas(img.width, img.height);
     const ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, img.width, img.height);
 
     const imageDataURL = canvas.toDataURL("image/jpeg");
-    console.log("aldı3");
-    pdf.addImage(imageDataURL, "JPEG", 10, 10);
-    console.log("aldı4");
+    pdf.addImage(imageDataURL, "JPEG", 0, 0, 210, 210);
     pdf.addPage();
   }
   return pdf.output("arraybuffer");
